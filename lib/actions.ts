9be0e70aac2825403697c36053b29038616a2a22
Andre Gallegos <andre.gallegos@tecsup.edu.pe>
2023-06-29 15:32:37 -0500

    Save image to Cloudinary. Missing to save in Graphql. Finish the Modal and FormField

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,4 +1,9 @@
-import { createUserMutation, getUserQuery } from "@/graphql";
+import { ProjectForm } from "@/common.types";
+import {
+  createProjectMutation,
+  createUserMutation,
+  getUserQuery,
+} from "@/graphql";
 import { GraphQLClient } from "graphql-request";
 
 const isProduction = process.env.NODE_ENV === "production";
@@ -13,6 +18,29 @@ const serverUrl = isProduction
   : "http://localhost:3000";
 
 const client = new GraphQLClient(apiUrl);
+
+export const fetchToken = async () => {
+  try {
+    const response = await fetch(`${serverUrl}/api/auth/token`);
+    return response.json();
+  } catch (err) {
+    throw err;
+  }
+};
+
+const uploadImage = async (imagePath: string) => {
+  try {
+    const response = await fetch(`${serverUrl}/api/upload`, {
+      method: "POST",
+      body: JSON.stringify({
+        path: imagePath,
+      }),
+    });
+    return response.json();
+  } catch (error) {
+    throw error;
+  }
+};
 
 const makeGraphqlRequest = async (query: string, variables: any) => {
   try {
@@ -38,4 +66,28 @@ export const createUser = (name: string, email: string, avatarUrl: string) => {
   };
 
   return makeGraphqlRequest(createUserMutation, variables);
+};
+
+export const createNewProject = async (
+  form: ProjectForm,
+  creatorId: string,
+  token: string
+) => {
+  const imageUrl = await uploadImage(form.image);
+
+  if (!imageUrl.url) {
+    client.setHeader("Authorization", `Bearer ${token}`);
+
+    const variables = {
+      input: {
+        ...form,
+        image: imageUrl.url,
+        createdBy: {
+          link: creatorId,
+        },
+      },
+    };
+
+    return makeGraphqlRequest(createProjectMutation, variables);
+  }
 };
